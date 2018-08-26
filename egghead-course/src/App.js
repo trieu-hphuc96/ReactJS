80b8513e4dad3665fc8267ed0f0e8d144195bc39
LAPTOP-TM9CEKV9\Phuc <trieu.hphuc96@yahoo.com.vn>
2018-08-26 19:22:43 +0700

    Up code for Lesson 13 14 15 16 on Egghead - Start Learning React

--- egghead-course/src/App.js
+++ egghead-course/src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import './App.css';
 
 class App extends Component {
 
-  mountLifeCircle(){
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+      input: '/* add your jsx here */',
+      output: '',
+      err: '',
+    }
+  }
+
+  componentWillMount() {
+    fetch('https://swapi.co/api/people/?format=json')
+      .then(response => response.json())
+      .then(({ results: items }) => this.setState({ items }))
+  }
+
+  mountLifeCircle() {
     ReactDOM.render(<LifeCircle />, document.getElementById('lifeCircle'));
   }
 
@@ -11,9 +28,57 @@ class App extends Component {
     ReactDOM.unmountComponentAtNode(document.getElementById('lifeCircle'));
   }
 
+  filter(e) {
+    this.setState({ filter: e.target.value })
+  }
+
+  updateOutput(e){
+    let code = e.target.value;
+    try {
+      this.setState({
+        output: window.Babel
+        .transform(code, {presets: ['es2015','react']})
+        .code,
+        err: ''
+      })
+    } catch (error) {
+      this.setState({err: error.message})
+    }
+  }
+
   render() {
+    let items = this.state.items;
+    if (this.state.filter) {
+      items = items.filter(item =>
+        item.name.toLowerCase()
+          .includes(this.state.filter.toLowerCase()))
+    }
     return (
       <div>
+        <input type="text" onChange={this.filter.bind(this)}></input>
+        {items.map(item =>
+          <Person key={item.name} person={item}></Person>)}
+        <hr></hr>
+
+        <div>
+          <header>{this.state.err}</header>
+          <div className="container">
+          <textarea
+          onChange={this.updateOutput.bind(this)}
+          defaultValue={this.state.input}></textarea>
+          <pre>
+            {this.state.output}
+          </pre>
+          </div>
+        </div>
+        <hr></hr>
+
+        <div>
+          <Button>button</Button>
+          <hr></hr>
+          <LabelHOC>label</LabelHOC>
+        </div>
+
         <button onClick={this.mountLifeCircle.bind(this)}>Mount</button>
         <button onClick={this.unMountLifeCircle.bind(this)}>Un Mount</button>
       </div>
@@ -25,35 +90,99 @@ class LifeCircle extends Component {
   constructor() {
     super();
     this.state = {
-      val: 0
+      increasing: false
     }
   }
 
   update() {
-    this.setState({ val : this.state.val + 1})
+    ReactDOM.render(
+      <LifeCircle val={this.props.val +1} />,
+      document.getElementById('lifeCircle')
+    )
   }
 
   componentWillMount() {
     console.log('componentWillMount');
-    this.setState({m: 2})
+    // For lesson 12 this.setState({ m: 2 })
   }
 
   componentDidMount() {
     console.log('componentDidMount');
-    this.inc = setInterval(this.update.bind(this),500);
+    // For lesson 12 this.inc = setInterval(this.update.bind(this), 500);
   }
 
   componentWillUnmount() {
     console.log('componentWillUnmount');
-    clearInterval(this.inc);
+    // For lesson 12 clearInterval(this.inc);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({increasing: nextProps.val > this.props.val})
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.val % 5 === 0;
   }
 
   render() {
     console.log('render');
+    console.log(this.state.increasing);
     return (
-      <button onClick={this.update.bind(this)}>{this.state.val * this.state.m}</button>
+      <button onClick={this.update.bind(this)}>{this.props.val}</button>
     );
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log(`preveProps: ${prevProps.val}`);
+  }
 }
+
+LifeCircle.defaultProps = {val: 0};
+
+const Person = (props) => <h4>{props.person.name}</h4>
+
+const HOC = (InnerComponent) => class extends Component {
+  constructor(){
+    super();
+    this.state = {
+      count: 0
+    };
+  }
+
+  componentWillMount(){
+    console.log('HOC will mount');
+  }
+
+  updateCount(){
+    this.setState({count: this.state.count + 1})
+  }
+
+  render() {
+    return (
+      <InnerComponent
+      {...this.props}
+      {...this.state}
+      update={this.updateCount.bind(this)}
+      />
+    )
+  }
+}
+
+const Button = HOC((props) => <button onClick={props.update}>{props.children} - {props.count}</button>)
+
+class Label extends Component {
+  componentWillMount(){
+    console.log('label will mount');
+  }
+  render() {
+    return(
+      <label onMouseMove={this.props.update}>
+        {this.props.children} - {this.props.count}
+      </label>
+    )
+  }
+}
+
+const LabelHOC = HOC(Label)
 
 export default App;
